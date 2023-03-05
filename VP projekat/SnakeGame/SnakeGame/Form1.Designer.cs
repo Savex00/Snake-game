d3437276057168a69a5aea72e03091ffaa0106f8
Savex00 <92447552+Savex00@users.noreply.github.com>
2023-03-05 17:03:12 +0100

    Add files via upload

---
+++ VP projekat/SnakeGame/SnakeGame/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace SnakeGame
+{
+    partial class SnakeGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SnakeGame";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(301, 5);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Speed";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(297, 43);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(203, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.gamePanel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.gamePanel.Location = new System.Drawing.Point(13, 100);
+            this.gamePanel.Margin = new System.Windows.Forms.Padding(4);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(760, 554);
+            this.gamePanel.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(560, 5);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(563, 38);
+            this.nameBox.Margin = new System.Windows.Forms.Padding(4);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(209, 22);
+            this.nameBox.TabIndex = 5;
+            this.nameBox.Text = "Guest";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkSalmon;
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.scoreLabel);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(782, 101);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(378, 554);
+            this.panel2.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveBorder;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 93);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(338, 437);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(118, 57);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 27);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Leaderboard";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(126, 17);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(32, 35);
+            this.scoreLabel.TabIndex = 9;
+            this.scoreLabel.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 16);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 35);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Score:";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(879, 34);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 28);
+            this.startButton.TabIndex = 7;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(1008, 34);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(4);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(100, 28);
+            this.stopButton.TabIndex = 8;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SnakeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1172, 715);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gamePanel);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "SnakeGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SnakeGame";
+            this.Load += new System.EventHandler(this.SnakeGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ VP projekat/SnakeGame/SnakeGame/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class SnakeGame : Form
+    {
+        //Snake
+        PictureBox[] snakeParts;
+        int snakeSize = 5;
+        Point location = new Point(120, 120);
+        string direction = "Right";
+        bool chanchingDirection = false;
+
+        //Hrana
+        PictureBox food = new PictureBox();
+        Point foodLocation = new Point(0, 0);
+
+        //Baza podataka
+        
+        string connectionString = "Data Source=DESKTOP-LTCVGEH;Initial Catalog=data;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public SnakeGame()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //ako korisnik zeli da ponovo pokrene igru
+            gamePanel.Controls.Clear();
+            snakeParts = null;
+            scoreLabel.Text = "0";
+            snakeSize = 5;
+            direction = "Right";
+            location = new Point(120, 120);
+
+            //Pokretanje igre 
+            drawSnake();
+            drawFood();
+
+
+            timer1.Start();
+
+            //onemogucavanje nekih kontrola kada je igra zavrsena
+            trackBar1.Enabled= false;
+            startButton.Enabled= false;
+            nameBox.Enabled= false;
+
+            //omogucene kontole na nakon sto je igra zavrsena
+            stopButton.Enabled = true;
+
+        }
+
+        private void drawSnake()
+        {
+            snakeParts= new PictureBox[snakeSize];
+
+            //petlja koja "izcrtava zmiju"
+            for(int i = 0 ; i < snakeSize; i++)
+            {
+                snakeParts[i] = new PictureBox();
+                snakeParts[i].Size = new Size(15,15);
+                snakeParts[i].BackColor= Color.Green;
+                snakeParts[i].BorderStyle= BorderStyle.FixedSingle;
+                snakeParts[i].Location = new Point(location.X - (15 * i), location.Y);
+                gamePanel.Controls.Add(snakeParts[i]);
+            }
+        }
+
+        private void drawFood()
+        {
+            Random random = new Random();
+            int xRand = random.Next(38) * 15;
+            int yRand = random.Next(30) * 15;
+
+            bool isOnSnake = true;
+
+            //provera da li je zmijica jede hranu
+            while (isOnSnake)
+            {
+                for(int i = 0; i < snakeSize; i++)
+                {
+                    if (snakeParts[i].Location == new Point(xRand, yRand))
+                    {
+                        xRand = random.Next(38) * 15;
+                        yRand = random.Next(30) * 15;
+                    }
+                    else
+                    {
+                        isOnSnake = false;
+                    }
+                }
+            }
+
+            //iscrtavanje hrane
+            if(isOnSnake == false)
+            {
+                foodLocation = new Point(xRand,yRand);
+                food.Size = new Size(15, 15);
+                food.BackColor = Color.DimGray;
+                food.BorderStyle = BorderStyle.FixedSingle;
+                food.Location = foodLocation;
+                gamePanel.Controls.Add(food);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //promena intervala tajmera (pomocu kojeg se pravi da se zmija pokrece)
+            //zavisno od promene brzine
+            timer1.Interval = 501 - (5 * trackBar1.Value);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            move();
+        }
+
+        private void move()
+        {
+            Point point = new Point(0, 0);
+
+            //pomeranje zmijice zavisno od pravca
+            for (int i = 0; i < snakeSize; i++)
+            {
+                if (i == 0)
+                {
+                    point = snakeParts[i].Location;
+                    if (direction == "Left")
+                    {
+                        snakeParts[i].Location = new Point(snakeParts[i].Location.X - 15, snakeParts[i].Location.Y);
+                    }
+                    if (direction == "Right")
+                    {
+                        snakeParts[i].Location = new Point(snakeParts[i].Location.X + 15, snakeParts[i].Location.Y);
+                    }
+                    if (direction == "Top")
+                    {
+                        snakeParts[i].Location = new Point(snakeParts[i].Location.X, snakeParts[i].Location.Y - 15);
+                    }
+                    if (direction == "Down")
+                    {
+                        snakeParts[i].Location = new Point(snakeParts[i].Location.X, snakeParts[i].Location.Y + 15);
+                    }
+                }
+                else
+                {
+                    Point newPoint = snakeParts[i].Location;
+                    snakeParts[i].Location = point;
+                    point = newPoint;
+                }
+            }
+
+            //zmijica jede hranu
+            if (snakeParts[0].Location == foodLocation)
+            {
+                eatFood();
+                drawFood();
+            }
+
+            //ako udari od ivicu
+            if (snakeParts[0].Location.X < 0 || snakeParts[0].Location.Y < 0 || snakeParts[0].Location.X >= 570 || snakeParts[0].Location.Y >= 570)
+            {
+                stopGame();
+            }
+
+            //ako udari samu sebe 
+            for(int i = 3; i < snakeSize; i++)
+            {
+                if (snakeParts[0].Location == snakeParts[i].Location)
+                {
+                    stopGame();
+                }
+            }
+
+            chanchingDirection = false;
+        }
+
+            //User input
+            protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if(keyData == (Keys.Up) && direction != "Down" && chanchingDirection != true)
+            {
+                direction = "Top";
+                chanchingDirection = true;
+            }
+            if (keyData == (Keys.Down) && direction != "Up" && chanchingDirection != true)
+            {
+                direction = "Down";
+                chanchingDirection = true;
+            }
+            if (keyData == (Keys.Left) && direction != "Right" && chanchingDirection != true)
+            {
+                direction = "Left";
+                chanchingDirection = true;
+            }
+            if (keyData == (Keys.Right) && direction != "Left" && chanchingDirection != true)
+            {
+                direction = "Right";
+                chanchingDirection = true;
+            }
+            
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void eatFood()
+        {
+            snakeSize++;
+
+            //cuvanje stare vrednosti i dobijanje nove(povecavanje zmijice)
+            PictureBox[] oldSnake = snakeParts;
+            gamePanel.Controls.Clear();
+            snakeParts = new PictureBox[snakeSize];
+
+            for(int i = 0; i < snakeSize; i++)
+            {
+                snakeParts[i] = new PictureBox();
+                snakeParts[i].Size = new Size(15, 15);
+                snakeParts[i].BackColor = Color.Green;
+                snakeParts[i].BorderStyle = BorderStyle.FixedSingle;
+
+                if(i == 0)
+                {
+                    snakeParts[i].Location = foodLocation;
+                }
+                else
+                {
+                    snakeParts[i].Location = oldSnake[i - 1].Location;
+                }
+
+                gamePanel.Controls.Add(snakeParts[i]);
+            }
+
+            //Updejtovanje skora
+            int currentScore = Int32.Parse(scoreLabel.Text);
+            int newScore = currentScore + 10;
+            scoreLabel.Text = newScore +""; 
+        }
+
+        private void stopGame()
+        {
+            timer1.Stop();
+            trackBar1.Enabled = true;
+            stopButton.Enabled = false;
+            startButton.Enabled = true;
+            nameBox.Enabled = true;
+
+            //Game over labela
+            Label over = new Label();
+            over.Text = "Game\nOver";
+            over.ForeColor= Color.White;
+            over.Font = new Font("Arial", 100, FontStyle.Bold);
+            over.Size = over.PreferredSize;
+            over.TextAlign = ContentAlignment.MiddleCenter;
+
+            int X = gamePanel.Width / 2 - over.Width / 2;
+            int Y = gamePanel.Height / 2 - over.Height / 2;
+            over.Location = new Point(X, Y);
+
+            gamePanel.Controls.Add(over);
+            over.BringToFront();
+
+            //Dodavanje poena u bazu
+            addCurrentScoreToDatabase();
+            updateScoreBoard();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            stopGame();
+        }
+
+        private void updateScoreBoard()
+        {
+            //Uzimanje poena iz baze i njihovo prikazivanje na data gridu
+            String query = "SELECT Date,Name,Scores FROM scores";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+
+                var ds = new DataSet();
+                adapter.Fill(ds);
+
+                dataGridView1.DataSource = ds.Tables[0];
+
+                dataGridView1.Columns[0].AutoSizeMode= DataGridViewAutoSizeColumnMode.AllCells;
+                dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+
+                dataGridView1.Sort(this.dataGridView1.Columns[0] ,ListSortDirection.Descending);
+
+            }
+
+
+        }
+
+        private void addCurrentScoreToDatabase()
+        {
+            string query = "INSERT INTO scores(Date,Name,Scores) VALUES(@Date,@Name,@Scores);";
+
+            using (SqlConnection con = new SqlConnection(connectionString)) 
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                cmd.Parameters.Add("@Date", SqlDbType.DateTime).Value = DateTime.Now;
+                cmd.Parameters.Add("@Name", SqlDbType.VarChar).Value = nameBox.Text;
+                cmd.Parameters.Add("@Scores", SqlDbType.Int).Value = scoreLabel.Text;
+
+                try 
+                { 
+                    con.Open();
+                    cmd.ExecuteNonQuery(); 
+                    con.Close();
+                }
+                catch (Exception ex) { 
+                    ex.ToString();
+                    Console.WriteLine(ex.ToString());
+                }
+                
+
+
+            }
+        }
+
+        private void SnakeGame_Load(object sender, EventArgs e)
+        {
+            updateScoreBoard();
+        }
+
+    }
+}
+
